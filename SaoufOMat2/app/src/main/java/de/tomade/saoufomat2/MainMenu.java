97267e7d94453e8b2fc29df1ded1f05765043a60
Dennis Rieke <dennis.rieke.93@gmail.com>
2016-03-08 00:36:42 +0100

    Grundprojekt erstellt

---
+++ SaoufOMat2/app/src/main/java/de/tomade/saoufomat2/MainMenu.java
@@ -0,0 +1,13 @@
+package de.tomade.saoufomat2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
