88b35543a6a5cacfe07c80b4b73f5f7d482f931c
Dennis Rieke <dennis.rieke.93@gmail.com>
2016-03-08 19:31:21 +0100

    Hauptmenü Hintergrundbild und ersten Button eingefügt

--- SaoufOMat2/app/src/main/java/de/tomade/saoufomat2/MainMenu.java
+++ SaoufOMat2/app/src/main/java/de/tomade/saoufomat2/MainMenu.java
@@ -1,13 +1,42 @@
 package de.tomade.saoufomat2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        ((ImageButton)findViewById(R.id.startButton)).setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        ImageButton view = (ImageButton) v;
+                        view.setImageResource(R.drawable.start_button_pressed);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:
+
+                        // Your action here on button click
+
+                    case MotionEvent.ACTION_CANCEL: {
+                        ImageButton view = (ImageButton) v;
+                        view.setImageResource(R.drawable.start_button);
+                        view.invalidate();
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
     }
 }
